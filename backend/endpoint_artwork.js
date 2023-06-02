a4a8d5e49c93618731243239f4bbf2e63b9d82cc
kayjirat <kayjirat12@gmail.com>
2023-06-02 21:20:37 +0700

    edit

--- backend/endpoint_artwork.js
+++ backend/endpoint_artwork.js
@@ -2,21 +2,26 @@ const express = require("express");
 const router = express.Router();
 
 module.exports = async (req, res) => {
-        const artworkId = req.params.artworkId;
-        console.log(artworkId);
-        connection.query("SELECT * FROM artwork WHERE artworkId = ?", [artworkId], (err, result) => {
-            if (err) {
-                console.log(err);
-                res.status(500).send("Error retrieving artwork id");
-              } else if (result.length === 0) {
-                res.status(404).send("Artwork not found");
-              } else {
-                res.json({
-                  success: true,
-                  message: "Found",
-                  user: result,
-                });
-              }
+  const artworkId = req.params.artworkId;
+  const sqlSelect = "SELECT * FROM artwork WHERE artworkId = ?";
+  connection.query(sqlSelect, [artworkId], (err, rows) => {
+    if (err) {
+      res.json({
+        error: "Error retrieving artwork information",
+      });
+    } else {
+      if (rows.length > 0) {
+        const artwork = rows[0];
+        res.json({
+          success: true,
+          artwork,
         });
-
+      } else {
+        res.json({
+          success: false,
+          message: "Artwork not found",
+        });
+      }
+    }
+  });
 };
